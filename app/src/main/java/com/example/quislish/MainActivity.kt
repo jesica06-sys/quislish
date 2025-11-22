6fd7916f306c7f954a7f71b53ce52dd7563ad79d
didinfakh <m.fakhruddin.arozi@gmail.com>
2025-11-22 14:17:52 +0700

    ubah menu dari activity ke fragment

--- app/src/main/java/com/example/quislish/MainActivity.kt
+++ app/src/main/java/com/example/quislish/MainActivity.kt
@@ -2,36 +2,10 @@ package com.example.quislish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.content.Intent
-import com.example.quislish.HomeActivity
-import com.example.quislish.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHome = findViewById<LinearLayout>(R.id.navHome)
-        val navLeaderboards = findViewById<LinearLayout>(R.id.navLeaderboards)
-        val navLesson = findViewById<LinearLayout>(R.id.navLesson)
-        val navProfile = findViewById<LinearLayout>(R.id.navProfile)
-
-        navHome.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-        }
-
-        navLeaderboards.setOnClickListener {
-            startActivity(Intent(this, LeaderboardsActivity::class.java))
-        }
-
-        navLesson.setOnClickListener {
-            startActivity(Intent(this, LessonActivity::class.java))
-        }
-
-        navProfile.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
     }
 }
