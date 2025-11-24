9b86911ac18706e6e0189cf491ca34a85a6781cf
didinfakh <m.fakhruddin.arozi@gmail.com>
2025-11-25 06:47:55 +0700

    perbaiki error import

--- app/src/main/java/com/example/quislish/WelcomeActivity.kt
+++ app/src/main/java/com/example/quislish/WelcomeActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.quislish.home.HomeActivity
+import com.example.quislish.navigasi.HomeActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
