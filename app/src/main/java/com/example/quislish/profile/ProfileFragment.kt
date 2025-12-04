068b428a32907dcc7d248548380857e2bd0fed8d
karlitacute <karlitawd9@gmail.com>
2025-12-05 00:15:22 +0700

    profil fragment

--- app/src/main/java/com/example/quislish/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/quislish/profile/ProfileFragment.kt
@@ -15,10 +15,20 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Title (boleh tetap)
         val title = view.findViewById<TextView>(R.id.tvAppName)
         title.text = "Profile Fragment"
+
+        // 1. Ambil username dari SharedPreferences
+        val sharedPref = requireActivity().getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+        val username = sharedPref.getString("username", "User")
+
+        // 2. Tampilkan ke TextView profil
+        val tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+        tvUsername.text = username
+
         return view
     }
 }
